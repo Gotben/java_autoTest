d18590a2170b7926911ad59714db838e4959f5d9
Gotben <gotbanzot@gmail.com>
2024-07-03 16:14:52 +0300

    Добавлены новые функции

--- sandbox/src/main/java/ru/autotestingMy/java_autoTest/sandbox/test1.java
+++ sandbox/src/main/java/ru/autotestingMy/java_autoTest/sandbox/test1.java
@@ -3,7 +3,29 @@ package ru.autotestingMy.java_autoTest.sandbox;
 public class test1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        hello("world");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + t1(l));
+//        double l = 5;
+//        t1(5);
+//        System.out.println("Площадь квадрата " + l + " = " + t1(l));
+        double a = 7;
+        double b = 7;
+        System.out.println("Площадь прямоугольника сос торонами " + a + " и " + b + " = " + t2(a,b));
     }
 
+    public static void hello(String user) {
+        System.out.println("Hello, " +user+ "!");
+    }
+
+    public static double t1(double l) {
+        return l * l;
+
+    }
+
+    public static double t2(double a, double b) {
+        return a * b;
+    }
+    
 }
